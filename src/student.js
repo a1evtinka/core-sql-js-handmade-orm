1665c1fdd9b44b314ff5023a34a5b0af9eec8d5b
sergeyutkin <sergey.utkin@splyt.com>
2018-10-23 12:23:55 +0300

    first commit

---
+++ src/student.js
@@ -0,0 +1,31 @@
+const pool = require('./the-pool-of-client');
+
+module.exports = class Students {
+    static where(query) {
+
+    }
+
+    static find(id) {
+
+    }
+
+    static findByName(name) {
+
+    }
+
+    static all() {
+
+    }
+
+    save() {
+
+    }
+
+    update() {
+
+    }
+
+    delete() {
+
+    }
+}
